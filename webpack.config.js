9e756bdde7045a097ff078e8d87b2af2368a5998
github user IshinoJun <J-Ishino@outlook.jp>
2020-01-05 15:23:49 +0900

    intsall eslint-loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebPackPlugin = require('html-webpack-plugin');
+
 const outputPath = path.resolve(__dirname, 'dist');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
@@ -9,54 +10,61 @@ module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'main.js',
-    path: outputPath
+    path: outputPath,
   },
   module: {
     rules: [
       {
+        enforce: 'pre',
         test: /\.jsx?$/,
         exclude: /node_modules/,
-        loader: "babel-loader" },
+        loader: 'eslint-loader',
+      },
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      },
       {
         test: /\.(sc|c)ss$/,
-        use: [MiniCssExtractPlugin.loader,'css-loader','sass-loader']
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
       },
       {
         test: /\.(jpe?g|png|gif|svg|ico)$/i,
         loader: 'url-loader',
         options: {
           limit: 2048,
-          name: './images/[name].[ext]'
-        }
+          name: './images/[name].[ext]',
+        },
       },
       {
         test: /\.html$/,
-        loader: 'html-loader'
-      }
-    ]
+        loader: 'html-loader',
+      },
+    ],
   },
   devServer: {
-    contentBase: outputPath
+    contentBase: outputPath,
   },
   plugins: [
     new HtmlWebPackPlugin({
       template: './src/index.html',
-      filename: './index.html'
+      filename: './index.html',
     }),
     new MiniCssExtractPlugin({
-      filename: '[name].[hash].css'
-    })
+      filename: '[name].[hash].css',
+    }),
   ],
   optimization: {
     minimizer: [
       new UglifyJsPlugin({
         uglifyOptions: {
           compress: {
-            drop_console: true
-          }
-        }
+            drop_console: true,
+          },
+        },
       }),
-      new OptimizeCSSAssetsPlugin()
+      new OptimizeCSSAssetsPlugin(),
     ],
   },
-}
+};
